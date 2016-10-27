be3f264b561786e4134d3706f7444cb304c92dc8
Romi <romana.pernischova@uzh.ch>
2016-10-27 09:01:20 +0200

    see me

--- app/js/rescuetime.js
+++ app/js/rescuetime.js
@@ -4,10 +4,23 @@
 
 function RescueTime() {}
 
+RescueTime.init = function(user){
+    //if(user === 'Romi'){
+    APIkey = "B63kYcoSejauNC0Fr2ULEfTTI_LGC2YPL9klMf_B"
+    perspective = "interval"
+    interval = "hour"
+    restrict = ""
+}
+
+var APIkey = ""
+var perspective = ""
+var interval = ""
+var restrict = ""
+
 RescueTime.getTracking = function($scope, $http){
   $http({
       method: "GET",
-      url: "https://www.rescuetime.com/anapi/data?key=B63kYcoSejauNC0Fr2ULEfTTI_LGC2YPL9klMf_B&perspective=interval&interval=hour&restrict_begin=2016-10-26T10&restrict_end=2016-10-26&format=json"
+      url: "https://www.rescuetime.com/anapi/data?key="&APIkey&"&perspective="&perspective&"&interval="&interval&"&restrict_begin="&restrict&"2016-10-26T10&restrict_end="&restrict&"2016-10-26&format=json"
   }).then(function successCallback(response){
       var data = response
   })
